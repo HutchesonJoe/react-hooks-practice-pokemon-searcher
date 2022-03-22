81b7f24df2dcf7856147f8b32ed8872f522ff91b
Joe <hutcheson2001@yahoo.com>
2022-03-22 17:08:54 -0400

    Completed assignment

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useState} from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+function PokemonForm({pList, setPList}) {
+  const [name, setName] = useState("")
+  const [hp, setHp] = useState("")
+  const [frontImage, setFrontImage] = useState("")
+  const [backImage, setBackImage] = useState("")
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          console.log("submitting form...");
-        }}
+        onSubmit={(e) => {
+          e.preventDefault()
+          fetch("http://localhost:3001/pokemon",{
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body : JSON.stringify({
+              "name": name,
+              "hp": hp,
+              "sprites" : {
+                "front" : frontImage,
+                "back" : backImage
+              }
+            })
+        })
+            .then(r=>r.json())
+            .then(data =>setPList([...pList, data]))
+      }}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={(e)=>setName(e.target.value)} />
+          <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={(e)=>setHp(e.target.value)}/>
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            onChange={(e)=>setFrontImage(e.target.value)}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            onChange={(e)=>setBackImage(e.target.value)}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({pList, setPList}) {
+
+  function handleSearch(e){
+    const thisPList = pList.filter((pok)=>{
+      return pok.name.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+    setPList(thisPList)
+  }
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" onChange={handleSearch}/>
         <i className="search icon" />
       </div>
     </div>
